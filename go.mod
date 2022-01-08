aa2d3f68c9bb874d702a2a46927933cb7697c7b0
Zoheb Alli Khan <understandingzak@gmail.com>
2022-01-09 00:05:58 +0530

    print cells done :)

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module fluffy
 
 go 1.16
+
+require (
+	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/kevinburke/ssh_config v1.1.0 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.1 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/net v0.0.0-20220107192237-5cfca573fb4d // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+)
--- stats.go
+++ stats.go
@@ -1,5 +1,178 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
+const outOfRange = 99999
+const daysInLastSixMonths = 183
+
+type column []int
+
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+}
+
+func countsDaysSinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
+}
+
+func calcOffset() int {
+	var offset int
+	weekday := time.Now().Weekday()
+
+	switch weekday {
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+	return offset
+}
+
+func printCommitsStats(commits map[int]int) {
+	keys := sortMapIntoSlice(commits)
+	cols := buildCols(keys, commits)
+	printCells(cols)
+}
+
+func sortMapIntoSlice(m map[int]int) []int {
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	return keys
+}
+
+func buildCols(keys []int, commits map[int]int) map[int]column {
+	cols := make(map[int]column)
+	col := column{}
+
+	for _, k := range keys {
+		week := int(k / 7)
+		dayinweek := k % 7
+
+		if dayinweek == 0 {
+			col = column{}
+		}
+
+		col = append(col, commits[k])
+
+		if dayinweek == 6 {
+			cols[week] = col
+		}
+	}
+
+	return cols
+}
+
+func printCells(cols map[int]column) {
+	printMonths()
+	for j := 6; j >= 0; j-- {
+		for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				if i == 0 && j == calcOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCells(0, false)
+		}
+		fmt.Print("\n")
+	}
+}
+
+func fillCommits(email string, path string, commits map[int]int) map[int]int {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		panic(err)
+	}
+
+	ref, err := repo.Head()
+	if err != nil {
+		panic(err)
+	}
+
+	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
+	if err != nil {
+		panic(err)
+	}
+
+	offset := calcOffset()
+	err = iterator.ForEach(func(c *object.Commit) error {
+		daysAgo := countsDaysSinceDate(c.Author.When) + offset
+
+		if c.Author.Email != email {
+			return nil
+		}
+
+		if daysAgo != outOfRange {
+			commits[daysAgo]++
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return commits
+}
+func processRepositories(email string) map[int]int {
+	filePath := getDotfilePath()
+	repos := parseFileLinesToSlice()
+	daysInMap := daysInLastSixMonths
+
+	commits := make(map[int]int, daysInMap)
+	for i := daysInMap; i > 0; i-- {
+		commits[i] = 0
+	}
+
+	for _, path := range repos {
+		commits = fillCommits(email, path, commits)
+	}
+
+	return commits
+}
+
 func stats(email string) {
-	print("Stats")
+	commits := processRepositories(email)
+	printCommitsStats(commits)
 }
